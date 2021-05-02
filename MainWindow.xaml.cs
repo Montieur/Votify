54843c2bf066d2bd6147113c14f5d1b3e3eccebe
Albert Przytuła <monter615@gmail.com>
2021-05-02 13:36:54 +0200

    pozdro

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Speech.Synthesis;
+using System.Collections.Generic;
+
+namespace VotifyTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var installedVoices = synth.GetInstalledVoices();
+
+            foreach(var voice in installedVoices)
+            {
+                voicesBox.Items.Add(voice.VoiceInfo.Name);
+            }
+            voicesBox.SelectedIndex = 0;
+        }
+
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak(spokenText.Text);
+        }
+
+        private void popupButton_Click(object sender, RoutedEventArgs e)
+        {
+            Popup popup = new Popup();
+            popup.Show();
+        }
+
+    }
+}
---
+++ Popup.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace VotifyTest
+{
+    /// <summary>
+    /// Interaction logic for Popup.xaml
+    /// </summary>
+    public partial class Popup : Window
+    {
+        double SCREEN_WIDTH = SystemParameters.PrimaryScreenWidth;
+        double SCREEN_HEIGHT = SystemParameters.PrimaryScreenHeight;
+        int i = 0;
+        bool moveUp = true;
+
+        DispatcherTimer timer = new DispatcherTimer()
+        {
+            Interval = TimeSpan.FromTicks(10000)
+        };
+
+        public Popup()
+        {
+            InitializeComponent();
+            Top = SCREEN_HEIGHT;
+            Left = SCREEN_WIDTH - Width;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            //moveAnimation();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (moveUp)
+            {
+                i += 10;
+                Top -= 10;
+                if (i >= Height) moveUp = false;
+            }
+            else
+            {
+                i -= 10;
+                Top += 10;
+                if (i <= 0) moveUp = true;
+            }   
+        }
+
+    }
+}
