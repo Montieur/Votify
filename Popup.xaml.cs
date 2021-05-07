c224a471e304d37e408ac04578b900e81606bc5d
Paweł Podlasin <PudliszkaStudio@gmail.com>
2021-05-07 19:46:06 +0200

    Dodanie czytania do Popup

--- Popup.xaml.cs
+++ Popup.xaml.cs
@@ -2,6 +2,8 @@
 using System.Media;
 using System.Windows;
 using System.Windows.Threading;
+using System.Speech.Synthesis;
+using System.Collections.Generic;
 
 namespace VotifyTest
 {
@@ -10,6 +12,7 @@ namespace VotifyTest
     /// </summary>
     public partial class Popup : Window
     {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
         double SCREEN_WIDTH = SystemParameters.PrimaryScreenWidth;
         double SCREEN_HEIGHT = SystemParameters.PrimaryScreenHeight;
         int TimeDisplayNotices;
@@ -17,11 +20,12 @@ namespace VotifyTest
         {
             InitializeComponent();
             SystemSounds.Beep.Play();
+            synth.SetOutputToDefaultAudioDevice();
             this.TimeDisplayNotices = TimeDisplayNotices;
             textBlockTheme.Text = Theme;
             textBlockDescription.Text = Descroption;
-            Top = SCREEN_HEIGHT;
-            Left = SCREEN_WIDTH - Width;
+            this.Top = SCREEN_HEIGHT; 
+            this.Left = SCREEN_WIDTH - Width;
             MoveUp(10);
         }
 
@@ -31,8 +35,8 @@ namespace VotifyTest
             timer.Interval = TimeSpan.FromTicks(10000);
 
             timer.Tick += (s, e) => {
-                Top -= step;
-                if(Top < SCREEN_HEIGHT - Height)
+                this.Top -= step;
+                if(this.Top < SCREEN_HEIGHT - Height)
                 {
                     Wait(TimeDisplayNotices, step);
                     timer.Stop();
@@ -44,6 +48,7 @@ namespace VotifyTest
 
         private void Wait(int freezeTime, int step)
         {
+            synth.Speak(generateSpeechText(textBlockTheme.Text,textBlockDescription.Text));
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(freezeTime);
             timer.Tick += (s, e) => {
@@ -51,6 +56,7 @@ namespace VotifyTest
                 timer.Stop();
             };
             timer.Start();
+            
         }
 
 
@@ -63,11 +69,29 @@ namespace VotifyTest
                 if (Top > SCREEN_HEIGHT)
                 {
                     timer.Stop();
-                    Close();
+                    this.Close();
                 } 
 
             };
             timer.Start();
+
+        }
+        private PromptBuilder generateSpeechText(string Thema,string Descroption)
+        {
+            PromptBuilder TextSpeech = new PromptBuilder(new System.Globalization.CultureInfo("pl-PL"));
+            TextSpeech.StartParagraph();
+            TextSpeech.StartSentence();
+            TextSpeech.AppendText("Powiadomienie!");
+            TextSpeech.EndSentence();
+            TextSpeech.StartSentence();
+            TextSpeech.AppendText(Thema);
+            TextSpeech.EndSentence();
+            TextSpeech.StartSentence();
+            TextSpeech.AppendText(Descroption);
+            TextSpeech.EndSentence();
+            TextSpeech.EndParagraph();
+
+            return TextSpeech;
         }
 
     }
